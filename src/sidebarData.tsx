5c1c1a8ed1f17a6b088862bf012cd213e813f203
Katsuya2022 <98100842+Katsuya2022@users.noreply.github.com>
2023-09-06 21:15:44 +0900

    サイドバーの実装

---
+++ src/sidebarData.tsx
@@ -0,0 +1,21 @@
+import HomeIcon from '@mui/icons-material/Home';
+import EmailIcon from '@mui/icons-material/Email';
+import HelpIcon from '@mui/icons-material/Help';
+
+export const sidebarData = [
+  {
+    title: "機能１",
+    icon: <HomeIcon />,
+    link: "/"
+  },
+  {
+    title: "機能２",
+    icon: <EmailIcon />,
+    link: "/BBB"
+  },
+  {
+    title: "機能３",
+    icon: <HelpIcon />,
+    link: "/CCC"
+  },
+]
